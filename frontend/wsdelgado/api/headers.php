de5607a3658b2750e545f312d33f82739e293805
amron <amron0131@gmail.com>
2026-04-29 11:09:07 +0800

    cors error

--- frontend/wsdelgado/api/headers.php
+++ frontend/wsdelgado/api/headers.php
@@ -1,6 +1,6 @@
 <?php
 // CORS headers
-header("Access-Control-Allow-Origin: http://localhost:3000");
+header("Access-Control-Allow-Origin: https://wsdelgado-tqq6-fdbbixo91-amronmuniz0131s-projects.vercel.app/");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Authorization");
 header("Access-Control-Allow-Credentials: true");
